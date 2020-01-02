dc306f07199efed11fa55444fc9f98c764bedd4d
Kalle Jokinen <kalle.jokinen@gmail.com>
2020-01-02 15:09:12 +0200

    Update metadata and passthrough xmltv.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const request = require('request');
 
 const {
   IPTV_DOMAIN,
@@ -7,31 +8,39 @@ const {
   IPTV_USERNAME,
   IPTV_PASSWORD,
   IPTV_PROTOCOL = 'http',
-  IPTV_EXTENSION = 'ts',
   PORT = '1234',
-  FILENAME = 'channels.m3u8',
 } = process.env;
 
-async function handler(_, res) {
-  const baseUri = `${IPTV_PROTOCOL}://${IPTV_DOMAIN}:${IPTV_PORT}`
-  const panelUri = `${baseUri}/panel_api.php?username=${IPTV_USERNAME}&password=${IPTV_PASSWORD}`
-  const { available_channels } = await fetch(panelUri).then(result => result.json());
+const baseUri = `${IPTV_PROTOCOL}://${IPTV_DOMAIN}:${IPTV_PORT}`;
 
-  res.write(`#EXTM3U\n`);
-  for (const channel_id in available_channels) {
-    const { name, stream_icon, category_name, stream_type, stream_id } = available_channels[channel_id];
+function channels(_, res) {
+  const panelUri = `${baseUri}/panel_api.php?username=${IPTV_USERNAME}&password=${IPTV_PASSWORD}`;
+  fetch(panelUri)
+    .then(result => result.json())
+    .then(({ available_channels }) => {
+      res.write(`#EXTM3U\n`);
+      for (const channel_id in available_channels) {
+        const { name, stream_icon, category_name, stream_type, stream_id, epg_channel_id, num } = available_channels[channel_id];
+    
+        res.write(`#EXTINF:0 channel-id="${num}" tvg-id="${epg_channel_id}" tvg-name="${name}" tvg-logo="${stream_icon}" channel-id="${name}" group-title="${category_name}|${stream_type}",${name}\n`);
+        res.write(`${baseUri}/${IPTV_USERNAME}/${IPTV_PASSWORD}/${stream_id}\n`);
+      }
+    
+      res.end();
+    });
+}
 
-    res.write(`#EXTINF:0 channel-id="${channel_id}" tvg-id="${name}" tvg-logo="${stream_icon}" channel-id="${name}" group-title="${category_name}|${stream_type}",${name}\n`);
-    res.write(`${baseUri}/${stream_type}/${IPTV_USERNAME}/${IPTV_PASSWORD}/${stream_id}.${IPTV_EXTENSION}\n`);
-  }
-
-  res.end();
+function xmltv(req, res) {
+  const xmltvUri = `${baseUri}/xmltv.php?username=${IPTV_USERNAME}&password=${IPTV_PASSWORD}`;
+  req.pipe(request(xmltvUri)).pipe(res);
 }
 
 function main() {
   const app = new express();
 
-  app.use(`/${FILENAME}`, handler);
+  app.use('/channels.m3u8', channels);
+  app.use('/xmltv.xml', xmltv);
+
   app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
   })
